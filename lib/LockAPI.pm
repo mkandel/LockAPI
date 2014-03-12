613d2dcb91931b0e7578a3dfdf73ead5988f338e
mkandel <marc.kandel.cpan@gmail.com>
2014-03-12 13:43:28 -0700

    wip

--- lib/LockAPI.pm
+++ lib/LockAPI.pm
@@ -1,6 +1,7 @@
 package LockAPI;
 use Mojo::Base 'Mojolicious';
 
+our $VERSION = '0.01';
 # This method will run once at server start
 sub startup {
   my $self = shift;
@@ -16,3 +17,35 @@ sub startup {
 }
 
 1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+LockAPI - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use LockAPI;
+
+=head1 DESCRIPTION
+
+LockAPI is
+
+=head1 AUTHOR
+
+Marc Kandel E<lt>mkandel@ariba.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Marc Kandel
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
