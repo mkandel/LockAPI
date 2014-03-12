82371423e689ce71ae2e24f5a7b980922488096b
Marc Kandel <mkandel@ariba.com>
2014-03-11 17:43:24 -0700

    initial commit

---
+++ lib/LockAPI.pm
@@ -0,0 +1,39 @@
+package LockAPI;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+LockAPI - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use LockAPI;
+
+=head1 DESCRIPTION
+
+LockAPI is
+
+=head1 AUTHOR
+
+Marc Kandel E<lt>mkandel@ariba.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Marc Kandel
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
