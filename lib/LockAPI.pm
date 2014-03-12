d874c50b0a741412465715ffbb0b01faeb072732
mkandel <marc.kandel.cpan@gmail.com>
2014-03-12 13:49:07 -0700

    perltidy

--- lib/LockAPI.pm
+++ lib/LockAPI.pm
@@ -2,18 +2,19 @@ package LockAPI;
 use Mojo::Base 'Mojolicious';
 
 our $VERSION = '0.01';
+
 # This method will run once at server start
 sub startup {
-  my $self = shift;
+    my $self = shift;
 
-  # Documentation browser under "/perldoc"
-  $self->plugin('PODRenderer');
+    # Documentation browser under "/perldoc"
+    $self->plugin('PODRenderer');
 
-  # Router
-  my $r = $self->routes;
+    # Router
+    my $r = $self->routes;
 
-  # Normal route to controller
-  $r->get('/')->to('example#welcome');
+    # Normal route to controller
+    $r->get('/')->to('example#welcome');
 }
 
 1;
