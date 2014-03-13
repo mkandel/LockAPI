3aa4c3d385bb7d4ce8d6466e8dd846268f90bd64
mkandel <marc.kandel.cpan@gmail.com>
2014-03-12 17:41:57 -0700

    wip

--- script/create_db.pl
+++ script/create_db.pl
@@ -2,10 +2,20 @@
 use strict;
 use warnings;
 
+use Data::Dumper;
 use DBI;
-my $db    = 'LockDB';
-my $dbh   = DBI->connect("dbi:SQLite:dbname=$db","","");
+use Getopt::Long;
+Getopt::Long::Configure qw/bundling no_ignore_case/;
+
+my $debug = 0;
+my $db    = 'data/LockDB.sqlite';
+my $dbh   = DBI->connect("dbi:SQLite:dbname=$db","","", { RaiseError => 1}) or die $DBI::errstr;
 my $table = 'locks';
+my $full  = "$db\.$table";
+
+GetOptions(
+    "debug|d"        => \$debug,
+);
 
 my %fields = (
     'lock_id'   => { 'type' => 'INTEGER', 'key' => 1, 'null' => 0, 'unique' => 1, },
@@ -15,6 +25,64 @@ my %fields = (
     'created'   => { 'type' => 'INTEGER', 'key' => 1, 'null' => 0, 'unique' => 0, },
     'expires'   => { 'type' => 'INTEGER', 'key' => 1, 'null' => 0, 'unique' => 0, },
 );
+
+$dbh->do( "DROP TABLE IF EXISTS $table" );
+
+my $sql = <<EOT;
+CREATE TABLE $table (
+EOT
+
+foreach my $field ( keys %fields ){
+    $sql .= $field . ' ' . $fields{ $field }->{'type'} . ",\n";
+    if ( $field eq 'lock_id' ){
+        ## This is the primary key, needs special handling:
+        ## strip the ',\n'
+        $sql =~ s/,\n$//;
+        ## Add the special stuff and the ',\n'
+        $sql .= " PRIMARY KEY AUTOINCREMENT,\n"
+    }
+}
+## sqlite fails if there's an extra , ... -------------------------^
+$sql =~ s/,\n$/\n/;
+
+$sql .= ")\n";  ## Add the ')'
+
+print "Running SQL:\n$sql\n" if $debug;
+$sql =~ s/\n//g;
+
+$dbh->do( $sql ) unless $debug;
+
+if ( $DBI::err ){
+    print "ERROR> ", $DBI::errstr, "\n";
+} else {
+    print "SQL Successful!\n";
+    #print "SQL Successful!\n" if $debug;
+}
+
+=pod
+
+my $sth = $dbh->prepare( $sql );
+print Dumper $dbh if $debug;
+print Dumper $sth if $debug;
+
+$dbh->{'RaiseError'} = 1;
+$sth->{'RaiseError'} = 1;
+
+$sth->execute unless $debug;
+$sth->finish;
+
+print Dumper $sth if $debug;
+
+if ( $dbh->err() ){
+    print "ERROR> ", $dbh->errstr(), "\n";
+} else {
+    print "SQL Successful!\n" if $debug;
+}
+
+=cut
+
+## DETACH DATABASE $db
+$dbh->disconnect();
 
 __END__
 
