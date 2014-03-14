41f59b054a300ffc121e73dbca1a472715a8ae3a
mkandel <marc.kandel.cpan@gmail.com>
2014-03-14 15:45:58 -0700

    wip

--- bin/locker.pl
+++ bin/locker.pl
@@ -1,4 +1,95 @@
 #!/usr/local/bin/perl -w
+use strict;
+use warnings;
+
+use Carp;
+use Getopt::Long;
+Getopt::Long::Configure qw/bundling no_ignore_case/;
+use Data::Dumper;
+# Some Data::Dumper settings:
+local $Data::Dumper::Useqq  = 1;
+local $Data::Dumper::Indent = 3;
+use Pod::Usage;
+
+use POSIX qw{ getlogin };
+use JSON;
+
+use Time::HiRes qw{ gettimeofday };
+my $start = gettimeofday;
+
+my $prog = $0;
+$prog =~ s/^.*\///;
+
+local $| = 1;
+
+my $debug  = 0;
+my $dryrun = 0;
+
+my $user = getlogin;
+my ( $product, $service, $host, $app, $expires, $extra_JSON );
+my $lock_srv = 'localhost';
+my $srv_port = 3000;
+
+my @actions = qw{ add delete list check modify };
+
+GetOptions(
+    "help|h"         => sub { pod2usage( 1 ); },
+    "debug|d"        => \$debug,
+    "dryrun|n"       => sub { $dryrun = 1; $debug = 1; },
+    "user|u=s"       => \$user,
+    "product|p=s"    => \$product,
+    "service|s=s"    => \$service,
+    "host|o=s"       => \$host,
+    "app|a=s"        => \$app,
+    "expires|e=i"    => \$expires,
+    "extra|x=s"      => \$extra_JSON,
+    "server|r=s"     => \$lock_srv,
+    "port|t=i"       => \$srv_port,
+);
+
+$lock_srv .= $srv_port ? ":$srv_port" : '';
+
+## We've parsed all the '-a|--arg' style args, the action should be left on ARGV
+my $action = shift @ARGV || pod2usage( -verbose => 1, -message => "action is a required argument!!" );
+
+## Validate action is valid:
+pod2usage( -verbose => 1, -message => "Invalid action '$action'!!" ) unless grep {/$action/} @actions;
+
+## Validate arguments:
+pod2usage( -verbose => 1, -message => "Missing argument 'user'!!"    ) unless $user;
+pod2usage( -verbose => 1, -message => "Missing argument 'product'!!" ) unless $product;
+pod2usage( -verbose => 1, -message => "Missing argument 'service'!!" ) unless $service;
+pod2usage( -verbose => 1, -message => "Missing argument 'host'!!"    ) unless $host;
+pod2usage( -verbose => 1, -message => "Missing argument 'app'!!"     ) unless $app;
+$expires = $expires || time;
+$extra_JSON = $extra_JSON || '';
+
+my $url = "http://$lock_srv/v1/$action/$service/$product/$host/$user/$app/$extra_JSON";
+
+if ( $debug ){
+    print "Action : '$action'\n";
+    print "Product: '$product'\n";
+    print "Service: '$service'\n";
+    print "Host   : '$host'\n";
+    print "App    : '$app'\n";
+    print "Expires: '$expires'\n";
+    print "URL    : '$url'\n";
+    print "\n";
+}
+
+END{
+    if ( $debug ){
+        my $run_time = gettimeofday() - $start;
+        print "$prog ran for ";
+        if ( $run_time < 60 ){
+            print "$run_time seconds.\n";
+        } else {
+            use integer;
+            print $run_time / 60 . " minutes " . $run_time % 60 
+                . " seconds ($run_time seconds).\n";
+        }
+    }
+}
 
 =head1 NAME
 
@@ -6,11 +97,20 @@ locker.pl - commandline client for LockAPI
 
 =head1 SYNOPSIS
 
-locker.pl [options] 
+locker.pl <action> [options] 
             
 =head1 OPTIONS
 
 =over
+
+=item B<<action>>
+
+One of:
+add
+check
+delete
+list
+modify
 
 =item B<--help|-h>
 
@@ -72,48 +172,6 @@ This code is released as Apathyware:
 
 =cut
 
-use strict;
-use warnings;
-
-use Carp;
-use Getopt::Long;
-Getopt::Long::Configure qw/bundling no_ignore_case/;
-use Data::Dumper;
-# Some Data::Dumper settings:
-local $Data::Dumper::Useqq  = 1;
-local $Data::Dumper::Indent = 3;
-use Pod::Usage;
-
-use POSIX qw{ getlogin };
-use JSON;
-
-local $| = 1;
-
-my $debug  = 0;
-my $dryrun = 0;
-
-my $user = getlogin;
-my ( $product, $service, $host, $app, $expires );
-my $extra_JSON;
-my %extra;
-
-GetOptions(
-    "help|h"         => sub { pod2usage( 1 ); },
-    "debug|d"        => \$debug,
-    "dryrun|n"       => sub { $dryrun = 1; $debug = 1; },
-    "user|u=s"       => \$user,
-    "product|p=s"    => \$product,
-    "service|s=s"    => \$service,
-    "host|o=s"       => \$host,
-    "app|a=s"        => \$app,
-    "expires|e=i"    => \$expires,
-    "extra|x=s"      => \$extra_JSON,
-);
-
-my $prog = $0;
-$prog =~ s/^.*\///;
-
-
 __END__
 
 service    - logical environment (dev3/load3)
@@ -123,3 +181,4 @@ user       - user requesting the lock
 app        - application requesting the lock.  could possibly be the same as 'product' but not in the case of Ops Tools
 extra      - optional hash of metadata relating to the lock
 expires    - time lock expires.  if no time given, default to some # of hours from 'add' time
+
